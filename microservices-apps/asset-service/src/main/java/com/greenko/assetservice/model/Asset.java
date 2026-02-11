7ffa083f78bda626c6c55b45d8cf57a008545bb5
ramanujds <ramanujds9@gmail.com>
2026-02-11 18:51:39 +0530

    Add Flyway and Zipkin configuration, enhance actuator endpoints, and update asset table schema

--- microservices-apps/asset-service/src/main/java/com/greenko/assetservice/model/Asset.java
+++ microservices-apps/asset-service/src/main/java/com/greenko/assetservice/model/Asset.java
@@ -1,13 +1,16 @@
 package com.greenko.assetservice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
+@Table(name = "assets")
 public class Asset {
 
     @Id
--- microservices-apps/asset-service/src/main/resources/db/dev-migration/V1__create_asset_table.sql
+++ microservices-apps/asset-service/src/main/resources/db/dev-migration/V1__create_asset_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE asset
+CREATE TABLE assets
 (
     asset_id       VARCHAR(255) NOT NULL,
     asset_name     VARCHAR(255),
     type           VARCHAR(255),
-    installed_date date,
+    installed_date DATE,
     location       VARCHAR(255),
     status         VARCHAR(255),
     CONSTRAINT pk_asset PRIMARY KEY (asset_id)
