81fb34c89709b943176cbcd9fa095c0f5525127b
ramanujds <ramanujds9@gmail.com>
2026-02-13 18:26:03 +0530

    Add configuration for telemetry service, implement resilience patterns, and introduce global request filter

--- microservices-apps/telemetry-service/src/main/java/com/greenko/telemetryservice/clent/AssetServiceClient.java
+++ microservices-apps/telemetry-service/src/main/java/com/greenko/telemetryservice/clent/AssetServiceClient.java
@@ -1,6 +1,9 @@
 package com.greenko.telemetryservice.clent;
 
 import com.greenko.telemetryservice.dto.AssetResponseDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface AssetServiceClient {
 
     @GetMapping("/api/assets/{assetId}")
+    @CircuitBreaker(name = "assetService", fallbackMethod = "getAssetByIdFallback")
+    @Retry(name = "assetServiceRetry")
+    @RateLimiter(name = "assetServiceRateLimiter")
     AssetResponseDto getAssetById(@PathVariable String assetId);
 
+    default AssetResponseDto getAssetByIdFallback(String assetId, Throwable throwable) {
+       System.err.println("Failed to fetch asset details for assetId: " + assetId + ". Error: " + throwable.getMessage());
+        return null;
+    }
 
 }
